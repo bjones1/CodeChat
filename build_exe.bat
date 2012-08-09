59f651c9ea43d4eccc39993396c8a3825d11f7c5
Bryan A. Jones <bjones@ece.msstate.edu>
2012-08-09 11:50:22 -0500

    - Applied patch to archive.py in PyInstaller from https://gist.github.com/991635 (starting at line 454). - Removed patches to sphinx, docutils - It works! No more .bat copying of data files

--- build_exe.bat
+++ build_exe.bat
@@ -1,6 +1,2 @@
 ..\pyinstaller-2.0\pyinstaller.py -y --noupx --additional-hooks-dir=pyinstaller_hooks code_chat.py
-xcopy /E /I C:\Python27\Lib\site-packages\Sphinx-1.1.3-py2.7.egg\sphinx\themes dist\code_chat\themes
-copy C:\Python27\Lib\site-packages\docutils\writers\html4css1\*.css dist\code_chat
-copy C:\Python27\Lib\site-packages\docutils\writers\html4css1\*.txt dist\code_chat
-copy C:\Python27\Lib\site-packages\docutils\parsers\rst\include\*.* dist\code_chat
 dist\code_chat\code_chat
