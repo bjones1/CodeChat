ed6ea0a88f15adcd0634b38bbdc7005ceacc3404
Bryan A. Jones <bjones@ece.msstate.edu>
2012-09-20 15:40:15 -0500

    - Add: build_dist assembles the instllation files then runs the installer

---
+++ build_dist.bat
@@ -0,0 +1,14 @@
+cd dist
+rmdir /q /s all
+mkdir all
+cd all
+hg clone ..\.. src
+rmdir /q /s src\.hg
+xcopy /E /I ..\code_chat bin
+xcopy /E /I ..\..\_build\html doc
+xcopy /E /I ..\..\template template
+copy ..\..\default.css template
+del template\conf.py.rst
+copy ..\..\installer.aip .
+installer.aip
+cd ..\..
