588d5888fe295fd464bbb051751757394dde4eae
Bryan A. Jones <bjones@ece.msstate.edu>
2013-03-11 16:53:59 -0500

    Add: First MruFiles test. PyTest is broken by this (???).

--- CodeChat/CodeChat_test.py
+++ CodeChat/CodeChat_test.py
@@ -4,6 +4,29 @@
 # ------------
 # This test bench exercises the CodeChat module. TODO!
 
+import sys
+from PyQt4 import QtGui, QtCore
+from CodeChat import MruFiles, form_class
+
+class CodeChatWindow(QtGui.QMainWindow, form_class):
+    def __init__(self):
+        # Let Qt and PyQt run their init first.
+        QtGui.QMainWindow.__init__(self)
+        self.setupUi(self)
+
+class TestMruFiles(object):
+    # The MruFiles object should work with nothing in the MRU list.
+    def test_mru_list_empty(self):
+        # Removing ``app =`` produces a ``QWidget: Must construct a QApplication before a QPaintDevice`` error.
+        app = QtGui.QApplication(sys.argv)
+        mw = CodeChatWindow()
+        settings = QtCore.QSettings("MSU BJones", "CodeChat_test")
+        # Remove all keys
+        for key in settings.allKeys():
+            settings.remove(key)
+        mru_files = MruFiles(mw, settings)
+        assert mru_files.get_mru_list() == []
+
 import pytest
 def main():
     # Run all tests -- see http://pytest.org/latest/usage.html#calling-pytest-from-python-code.
@@ -12,4 +35,6 @@ def main():
     #pytest.main('-k test_11')
 
 if __name__ == '__main__':
-    main()
+#    main()
+    a = TestMruFiles()
+    a.test_mru_list_empty()
