4452483a56a63f8cb1fd4c1a03b06a4b4e04da38
Bryan A. Jones <bjones@ece.msstate.edu>
2013-03-12 16:20:15 -0500

    Add: test_mru_double_insert. Refactor: Created insert_letters.

--- CodeChat/CodeChat_test.py
+++ CodeChat/CodeChat_test.py
@@ -34,13 +34,28 @@ class TestMruFiles(object):
         # There's no MRU file to open.
         assert self.mru_files.open_mru() == False
 
-    # Insert items. The most recently inserted item should be at the top, while the first insertion is at the end of the list.
-    def test_mru_insert_order(self):
+    # This helper method inserts 'a'...'j' into the MRU list.
+    def insert_letters(self):
         # Create a list ['a', 'b', ... 'j'], 10 items long.
         file_list = [chr(ord('a') + i) for i in range(10)]
         # Add these as files to the MRU list.
         for file_name in file_list:
             self.mru_files.add_file(file_name)
+        return file_list
+
+    # Insert items. The most recently inserted item should be at the top, while the first insertion is at the end of the list.
+    def test_mru_insert_order(self):
+        file_list = self.insert_letters()
         # Check the order.
         file_list.reverse()
         assert self.mru_files.get_mru_list() == file_list
+
+    def test_mru_double_insert(self):
+        file_list = self.insert_letters()
+        self.mru_files.add_file('a')
+        # Check the order.
+        file_list.reverse()
+        file_list = file_list[0:-1]
+        file_list.insert(0, 'a')
+        assert self.mru_files.get_mru_list() == file_list
+
