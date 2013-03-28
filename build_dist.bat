3f5b1e49646d8671dc7da9b48f42039db46e3b5b
Bryan A. Jones <bjones@ece.msstate.edu>
2013-03-28 10:48:49 -0500

    Fix: On a clean build, need to create the dist directory.

--- build_dist.bat
+++ build_dist.bat
@@ -8,6 +8,7 @@
 :
 : You should have received a copy of the GNU General Public License along with CodeChat.  If not, see <http://www.gnu.org/licenses/>.
 
+mkdir dist
 cd dist
 rmdir /q /s all
 mkdir all
