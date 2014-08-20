f68b4c39c5eb442588c4ff6a29db50c64562583d
Bryan A. Jones <bjones AT ece.msstate.edu>
2014-08-20 16:41:59 -0500

    Add: Sphinx template.

--- CodeChat/__init__.py
+++ CodeChat/__init__.py
@@ -26,4 +26,4 @@
 # Give the version number, which is read by :doc:`../setup.py` during packaging.
 # This is chosen following the `version convention
 # <http://packaging.python.org/en/latest/tutorial.html#version>`_.
-__version__ = '0.0.12a1'
+__version__ = '0.0.12a3'
