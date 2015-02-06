67ae843001e554a4ce8f9b82c5cd4d08f6996d1f
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-02-06 11:44:29 -0600

    Fix: Remove unused PyQt dependencies.      Modernize documentation style in LanguageSpecificOptions.py.

--- CodeChat/LanguageSpecificOptions.py
+++ CodeChat/LanguageSpecificOptions.py
@@ -4,68 +4,62 @@
 #
 #    This file is part of CodeChat.
 #
-#    CodeChat is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#    CodeChat is free software: you can redistribute it and/or modify it under
+#    the terms of the GNU General Public License as published by the Free
+#    Software Foundation, either version 3 of the License, or (at your option)
+#    any later version.
 #
-#    CodeChat is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#    CodeChat is distributed in the hope that it will be useful, but WITHOUT ANY
+#    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#    FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#    details.
 #
-#    You should have received a copy of the GNU General Public License along with CodeChat.  If not, see <http://www.gnu.org/licenses/>.
-
-# | Fix: Sphinx reports the error:
-# |  ``Extension error:``
-# |  ``Could not import extension CodeChat.CodeToRest (exception: No module named Qsci)``
-# | on the import below. ??? We only need this import for the CodeChat app, so work around it for now.
-try:
-    from PyQt4.Qsci import QsciLexerCPP, QsciLexerPython
-except ImportError as e:
-    QsciLexerCPP, QsciLexerPython = (None, None)
-
+#    You should have received a copy of the GNU General Public License along
+#    with CodeChat.  If not, see <http://www.gnu.org/licenses/>.
+#
 # ****************************************************************************************
 # LanguageSpecificOptions.py - assist in providing language-specific settings for CodeChat
 # ****************************************************************************************
 #
 # LanguageSpecificOptions
 # =======================
-# For each programming language supported, :meth:`set_language` specifies:
+# For each programming language supported, ``set_language`` specifies:
 #
-# .. attribute:: comment_string
-#
-#    The string indicating the beginning of a comment in the chosen programming language, or None if the CodeToRest process isn't supported. This must end in a space for the regular expression in format to work. The space also makes the output a bit prettier.
-#
-# .. attribute:: lexer
-#
-#    The QScintilla lexer to use, or None to disable syntax highlighting in the text pane
-#
-# .. class:: LanguageSpecificOptions()
+# comment_string:
+#    The string indicating the beginning of a comment in the chosen programming
+#    language, or None if the CodeToRest process isn't supported. This must end
+#    in a space for the regular expression in format to work. The space also
+#    makes the output a bit prettier.
 class LanguageSpecificOptions(object):
-    # .. attribute:: unique_remove_str
-    #
-    #    A unique string to mark lines for removal in HTML.
+    # A unique string to mark lines for removal in HTML.
     unique_remove_str = u'wokifvzohtdlm'
 
-    # .. attribute:: language_specific_options
-    #
-    #    A dict of language-specific options.
+    #  A dict of language-specific options.
     extension_to_options = {
-    ##  Pygments  lexer
-    ##  Extension  Comment string, QScintilla lexer
-      u'.c'      : (u'//',           QsciLexerCPP),
-      u'.cc'     : (u'//',           QsciLexerCPP),
-      u'.cpp'    : (u'//',           QsciLexerCPP),
-      u'.h'      : (u'//',           QsciLexerCPP),
-      u'.hh'     : (u'//',           QsciLexerCPP),
-      u'.hpp'    : (u'//',           QsciLexerCPP),
-      u'.py'     : (u'#',            QsciLexerPython),
-      u'.s'      : (u';',            None),
-      u'.php'    : (u'#',            None),
-      u'.m'      : (u'%',            None),
-      u'.bat'    : (u':',            None),
-      u'.ini'    : (u';',            None),
-      u'.iss'    : (u';',            None),
+    ##  Extension  Comment string
+        u'.c'      : u'//',
+        u'.cc'     : u'//',
+        u'.cpp'    : u'//',
+        u'.h'      : u'//',
+        u'.hh'     : u'//',
+        u'.hpp'    : u'//',
+        u'.py'     : u'#',
+        u'.s'      : u';',
+        u'.php'    : u'#',
+        u'.m'      : u'%',
+        u'.bat'    : u':',
+        u'.ini'    : u';',
+        u'.iss'    : u';',
+        u'.java'   : u'//',
     }
 
-    # .. method:: set_language(extension)
-    #
-    #    Sets the :class:`LanguageSpecificOptions` offered, where *extension* gives the extension for the desired language.
-    def set_language(self, extension):
-        # If the extension is unknown, then assign the comment string and lexer as None.
-        (self.comment_string, self.lexer) = self.extension_to_options.get(extension, (None, None))
+    def __init__(self):
+        # Start with an unspecified comment_string.
+        self.comment_string = None
+
+    # Sets the ``LanguageSpecificOptions`` offered.
+    def set_language(self,
+      # The file extension selecting the desired language.
+      extension):
+        # If the extension is unknown, then assign the comment string = None.
+        self.comment_string = self.extension_to_options.get(extension)
