3d29a41fabe21fd7eec0b9d09c26a579680adc83
Bryan A. Jones <bjones AT ece.msstate.edu>
2015-06-05 15:04:13 -0500

    Fix: Manually import what __init__ used to do for us.

--- conf.py
+++ conf.py
@@ -30,6 +30,7 @@ import sys, os
 import sphinx
 # Use the CodeChat version date in its generated documentation.
 import CodeChat
+import CodeChat.CodeToRestSphinx
 #
 # If extensions (or modules to document with autodoc) are in another directory, add these directories to sys.path here. If the directory is relative to the documentation root, use os.path.abspath to make it absolute, as shown here.
 ##sys.path.insert(0, os.path.abspath('.'))
