1e30701f2b1c1d59b859cc205f4b571cf4017ff5
acmitchell11 <acmitchell11@gmail.com>
2017-06-29 10:00:10 -0500

    Cleaned code and performed requested Changes Deleted file_info()

--- CodeChat/RestToCode.py
+++ CodeChat/RestToCode.py
@@ -52,7 +52,6 @@ from CodeChat.CommentDelimiterInfo import COMMENT_DELIMITER_INFO
 def find_file_ext(
   # See lang_.
   lang):
-    # TODO Make a test for finding the file extention.
     if lang in COMMENT_DELIMITER_INFO:
         # `find_lexer_class <http://pygments.org/docs/api/#pygments.lexers.find_lexer_class>`_
         # operates on the `name <http://pygments.org/docs/api/#pygments.lexer.Lexer.name>`_
@@ -67,57 +66,6 @@ def find_file_ext(
         return file_ext
     else:
         return None
-'''    found = 0
-    for longname, aliases, filename_patterns, mimetypes in get_all_lexers():
-        # Pick only filenames we have comment info for.
-        if longname in COMMENT_DELIMITER_INFO:
-            if longname == lang:
-                file_ext_raw = filename_patterns[0]
-                found = 1
-    # If we support the language, return the file extension.
-    # Else, return ``None``
-    # TODO do a for/else statement. use break to
-    if found == 1:
-        # Use only the ``.py`` part of ``*.py`` and similar.
-        file_ext_list = file_ext_raw.split('*', 1)
-        file_ext = file_ext_list[-1]
-        return file_ext
-    else:
-        return None'''
-# |
-#
-# _`file_info`: Gather the location of the file to be translated and the name of the language
-# that the file will be translated into.
-# This is the only part of the code that needs to be filled out by the user at runtime.
-#
-# The user can skip this by providing a language in the parameters of rest_to_code_file_.
-def file_info(
-  # See source_rst_path_.
-  source_rst_path,
-  # See out_path_.
-  out_path):
-    # If the source path has not been given as a parameter, obtain it at runtime from the user.
-    if source_rst_path is None:
-        file_name_raw = str(input('What is the path(including the name) of the file to be translated? '))
-        # This takes just the file location and name (it removes the extension)
-        file_name_list = file_name_raw.rsplit('.', 1)
-        source_rst_path = file_name_list[0]
-    # If source path has been given but output path has not, remove the ``.rst`` extension.
-    elif out_path is None:
-        file_name_list = source_rst_path.rsplit('.', 1)
-        source_rst_path = file_name_list[0]
-
-    new_file_language = str(input('What is the language you would like? '))
-    # This section allows the program to determine if we support the language
-    # that the user has input.
-    file_ext = None
-    while file_ext is None:
-        file_ext = find_file_ext(new_file_language)
-        if file_ext is not None:
-            break
-        new_file_language = str(input('We could not find that one. Please try again: '))
-
-    return [source_rst_path, new_file_language, file_ext]
 # |
 #
 # _`language_comment_type`: Allows the use of languages that have only inline comments or only block comments
@@ -159,28 +107,18 @@ def formulate_comment(
     # Grab the comment delimiters for the given language
     comment_delimiters = COMMENT_DELIMITER_INFO[lang]
     # Check to see what kinds of comments the language supports
-    comment_type = language_comment_type(comment_delimiters)
+    has_inline_comment, has_block_comment = language_comment_type(comment_delimiters)
     # Create an inline comment
-    if is_block_comment is False and comment_type[0] is True:
-        return formulate_inline_comment(line, comment_delimiters)
-    elif is_block_comment is True and comment_type[1] is False:
-        return formulate_inline_comment(line, comment_delimiters) # TODO create a test to check if this is right... I think it might only do the first line of the block comment and skip the rest.
+    if (not is_block_comment and has_inline_comment) or (is_block_comment and not has_block_comment):
+        # Stops the whole program if there is no inline comment delimiter and it is asking to use an inline comment.
+        assert has_inline_comment
+        # Formulates an inline comment
+        return '{} {}\n'.format(comment_delimiters[0], line)
     # Create a block comment
     else:
-        if is_block_comment is False:
+        if not is_block_comment:
             line_counter = None
         return formulate_block_comment(line, comment_delimiters, position, line_counter)
-# |
-#
-# _`formulate_inline_comment`: Formulates an inline comment
-def formulate_inline_comment(
-  # See line_.
-  line,
-  # See comment_delimiters_.
-  comment_delimiters):
-
-    f = '{} '.format(comment_delimiters[0]) + line + '\n'
-    return f
 # |
 #
 # _`formulate_block_comment`: Formulates a block comment one line at a time.
@@ -200,19 +138,19 @@ def formulate_block_comment(
     # space is not taken out in the Code to reST translation. If a space is added, it is no 
     # longer round trip stable. (It adds a space every time it is translated.)
     if line_counter is None:
-        f = '{} '.format(comment_delimiters[1]) + line + '{}'.format(comment_delimiters[2]) + '\n'
+        f = '{} {}{}\n'.format(comment_delimiters[1], line, comment_delimiters[2])
     # This is the regular block comment case.
     else:
         # It places the open comment delimiter in front of the first line of the comment,
         if position == line_counter:
-            f = '{} '.format(comment_delimiters[1]) + line + '\n'
+            f = '{} {}\n'.format(comment_delimiters[1], line)
         # It places a ``' * '`` in front of every other line, including the last line in the comment
         # for consistency and visual appeal.
         elif position > 0:
             f = ' * ' + line + '\n'
         # It places the closing comment delimiter at the end of the last line of the comment.
         else:
-            f = ' * ' + line + '{}'.format(comment_delimiters[2]) + '\n'
+            f = ' * {}{}\n'.format(line, comment_delimiters[2])
 
     return f
 #
@@ -226,27 +164,29 @@ def formulate_block_comment(
 # another language. Inputs a reST file, outputs a code file.
 def rest_to_code_file(
   # _`source_rst_path`: Path to a source reST file to process.
-  source_rst_path=None,
+  source_rst_path,
   # _`out_path`:Path to a destination code file to create. It will be overwritten if it
-  # already exists.
+  # already exists. If out_path_ is None, the output path will be the source_rst_path_
+  # but with the correct file extension for the given lang_
   out_path=None,
   # _`lang`: Specify the language that the reST will be translated into. This is the key to
   # the dictionary found in :doc:`CommentDelimiterInfo.py`
-  # Ex: ``'Python'`` and ``'C'``
-  lang=None,
+  # Ex: ``'Python'`` or ``'C'``
+  lang,
   # _`input_encoding`: Encoding to use for the input file. The default of None detects the encoding
   # of the input file.
   input_encoding=None,
   # _`output_encoding`: Encoding to use for the output file.
   output_encoding='utf-8'):
 
-    if lang is None:
-        file_name, lang, file_ext = file_info(source_rst_path, out_path)
-
-        if source_rst_path is None:
-            source_rst_path = '{}.rst'.format(file_name)
-        if out_path is None:
-            out_path = '{}{}'.format(file_name, file_ext)
+    if out_path is None:
+        # Find the file extension of the given language.
+        file_ext = find_file_ext(lang)
+        # Obtain the file path without the ``.rst`` extension.
+        file_name_list = source_rst_path.rsplit('.', 1)
+        file_name = file_name_list[0]
+        # Place the file extension on the file path.
+        out_path = '{}{}'.format(file_name, file_ext)
 
     # Use docutil's I/O classes to better handle and sniff encodings.
     #
