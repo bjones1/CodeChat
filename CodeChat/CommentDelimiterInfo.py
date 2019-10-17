ea80eca9c7ec5bc4c84c4503e1ab140982a842b9
Bryan A. Jones <bjones@ece.msstate.edu>
2019-10-17 15:50:17 -0500

    Clean: manually black.

--- CodeChat/CommentDelimiterInfo.py
+++ CodeChat/CommentDelimiterInfo.py
@@ -38,120 +38,120 @@ from pygments.lexers import get_all_lexers
 # mostly taken from the `Wikipedia page
 # <http://en.wikipedia.org/wiki/Comparison_of_programming_languages_(syntax)#Comments>`_.
 COMMENT_DELIMITER_INFO = {
-  # These languages have unit tests which pass
-  # ------------------------------------------
-  # See :doc:`../test/CodeToRest_test.py` for the tests.
-  ## Language name: inline, block opening, block closing
-  'C':              ( '//',     '/*',            '*/'),
-  'C++':            ( '//',     '/*',            '*/'),
-  'Java':           ( '//',     '/*',            '*/'),
-  'ActionScript':   ( '//',     '/*',            '*/'),
-  'ActionScript 3': ( '//',     '/*',            '*/'),
-  'C#':             ( '//',     '/*',            '*/'),
-  # Note: also has   ``/+``  ~  ``+/`` for nested block comments.
-  # (reST to code will use characters in tuple)
-  'D':              ( '//',     '/*',            '*/'),
-  'Go':             ( '//',     '/*',            '*/'),
-  'JavaScript':     ( '//',     '/*',            '*/'),
-  'Objective-C':    ( '//',     '/*',            '*/'),
-  'Rust':           ( '//',     '/*',            '*/'),
-  'Scala':          ( '//',     '/*',            '*/'),
-  'Swift':          ( '//',     '/*',            '*/'),
-  'verilog':        ( '//',     '/*',            '*/'),
-  'systemverilog':  ( '//',     '/*',            '*/'),
-  'Dart':           ( '//',     '/*',            '*/'),
-  'Juttle':         ( '//',     '/*',            '*/'),
-  'Objective-J':    ( '//',     '/*',            '*/'),
-  'TypeScript':     ( '//',     '/*',            '*/'),
-  'Arduino':        ( '//',     '/*',            '*/'),
-  'Clay':           ( '//',     '/*',            '*/'),
-  'CUDA':           ( '//',     '/*',            '*/'),
-  'eC':             ( '//',     '/*',            '*/'),
-  'MQL':            ( '//',     '/*',            '*/'),
-  'nesC':           ( '//',     '/*',            '*/'),
-  'Pike':           ( '//',     '/*',            '*/'),
-  'SWIG':           ( '//',     '/*',            '*/'),
-  'Vala':           ( '//',     '/*',            '*/'),
-  'Zephir':         ( '//',     '/*',            '*/'),
-  'Haxe':           ( '//',     '/*',            '*/'),
+# These languages have unit tests which pass
+# ------------------------------------------
+# See :doc:`../test/CodeToRest_test.py` for the tests.
+    ## Language name: inline, block opening, block closing
+    "C":              ( "//",     "/*",            "*/"),
+    "C++":            ( "//",     "/*",            "*/"),
+    "Java":           ( "//",     "/*",            "*/"),
+    "ActionScript":   ( "//",     "/*",            "*/"),
+    "ActionScript 3": ( "//",     "/*",            "*/"),
+    "C#":             ( "//",     "/*",            "*/"),
+    # Note: also has   ``/+``  ~  ``+/`` for nested block comments.
+    # (reST to code will use characters in tuple)
+    "D":              ( "//",     "/*",            "*/"),
+    "Go":             ( "//",     "/*",            "*/"),
+    "JavaScript":     ( "//",     "/*",            "*/"),
+    "Objective-C":    ( "//",     "/*",            "*/"),
+    "Rust":           ( "//",     "/*",            "*/"),
+    "Scala":          ( "//",     "/*",            "*/"),
+    "Swift":          ( "//",     "/*",            "*/"),
+    "verilog":        ( "//",     "/*",            "*/"),
+    "systemverilog":  ( "//",     "/*",            "*/"),
+    "Dart":           ( "//",     "/*",            "*/"),
+    "Juttle":         ( "//",     "/*",            "*/"),
+    "Objective-J":    ( "//",     "/*",            "*/"),
+    "TypeScript":     ( "//",     "/*",            "*/"),
+    "Arduino":        ( "//",     "/*",            "*/"),
+    "Clay":           ( "//",     "/*",            "*/"),
+    "CUDA":           ( "//",     "/*",            "*/"),
+    "eC":             ( "//",     "/*",            "*/"),
+    "MQL":            ( "//",     "/*",            "*/"),
+    "nesC":           ( "//",     "/*",            "*/"),
+    "Pike":           ( "//",     "/*",            "*/"),
+    "SWIG":           ( "//",     "/*",            "*/"),
+    "Vala":           ( "//",     "/*",            "*/"),
+    "Zephir":         ( "//",     "/*",            "*/"),
+    "Haxe":           ( "//",     "/*",            "*/"),
 
-  # Note: PHP allows ``#`` or ``//`` as an inline comment. We only support
-  # ``#``.
-  'PHP':            ( '#',      '/*',            '*/'),
-  # Block comments not tested.
-  'NASM':           (';', '%comment\n', '%endcomment'),
-  # PIC24 not tested.
-  'PIC24':          (';',       '/*',            '*/'),
-  # In Python, docstrings are treated as multi-line comments.
-  'Python':         ( '#',      '"""',          '"""'),
-  'Python 3':       ( '#',      '"""',          '"""'),
-  'CSS':            (  '',       '/*',           '*/'),
-  # This covers csh and sh as well. Wikipedia claims that ``<#`` ~ ``#>`` are
-  # block comments, but I don't see this anywhere in man bash. These aren't
-  # supported.
-  'Bash':           ( '#',        '',              ''),
-  'Tcsh':           ( '#',        '',              ''),
-  # The only valid comment type is ``rem``. Neither ``:`` or ``::`` are
-  # classified as a comment. Not that this must be a lower-case string, since it
-  # will be compared to a lowercased comment.
-  'Batchfile':      ( 'rem',      '',              ''),
-  'Matlab':         ( ('%', '...'), '%{',            '%}'),
-  'SQL':            ( '--',     '/*',            '*/'),
-  'PowerShell':     ( '#',      '<#',            '#>'),
-  # ``/*`` ~ ``*/`` not supported (Pygments doesn't lex these).
-  'GAS':            ( '#',      '/*',            '*/'),
-  'autohotkey':     ( ';',      '/*',            '*/'),
-  'Prolog':         ( '%',      '/*',            '*/'),
-  'AutoIt':         ( ';',     '#cs',           '#ce'),
+    # Note: PHP allows ``#`` or ``//`` as an inline comment. We only support
+    # ``#``.
+    "PHP":            ( "#",      "/*",            "*/"),
+    # Block comments not tested.
+    "NASM":           (";", "%comment\n", "%endcomment"),
+    # PIC24 not tested.
+    "PIC24":          (";",       "/*",            "*/"),
+    # In Python, docstrings are treated as multi-line comments.
+    "Python":         ( "#",      '"""',          '"""'),
+    "Python 3":       ( "#",      '"""',          '"""'),
+    "CSS":            (  "",       "/*",           "*/"),
+    # This covers csh and sh as well. Wikipedia claims that ``<#`` ~ ``#>`` are
+    # block comments, but I don"t see this anywhere in man bash. These aren"t
+    # supported.
+    "Bash":           ( "#",        "",              ""),
+    "Tcsh":           ( "#",        "",              ""),
+    # The only valid comment type is ``rem``. Neither ``:`` or ``::`` are
+    # classified as a comment. Not that this must be a lower-case string, since it
+    # will be compared to a lowercased comment.
+    "Batchfile":      ( "rem",      "",              ""),
+    "Matlab":         ( ("%", "..."), "%{",        "%}"),
+    "SQL":            ( "--",     "/*",            "*/"),
+    "PowerShell":     ( "#",      "<#",            "#>"),
+    # ``/*`` ~ ``*/`` not supported (Pygments doesn"t lex these).
+    "GAS":            ( "#",      "/*",            "*/"),
+    "autohotkey":     ( ";",      "/*",            "*/"),
+    "Prolog":         ( "%",      "/*",            "*/"),
+    "AutoIt":         ( ";",     "#cs",           "#ce"),
 
-  # `PODs <https://docs.perl6.org/language/pod>`_ not supported. Only single line comments
-  'Perl':           ( '#',        '',              ''),
-  # PODs not supported in Perl6, since they conflict with the new block-style
-  # comments: ``#`[`` ~ ``]``, or any other pair.
-  'Perl6':          ( '#',     "#'(",             ')'),
+    # `PODs <https://docs.perl6.org/language/pod>`_ not supported. Only single line comments
+    "Perl":           ( "#",        "",              ""),
+    # PODs not supported in Perl6, since they conflict with the new block-style
+    # comments: ``#`[`` ~ ``]``, or any other pair.
+    "Perl6":          ( "#",     "#'(",             ")"),
 
-  'Ruby':           ( '#',  '=begin',          '=end'),
-  'S':              ( '#',        '',              ''),
-  # `Bird style <https://wiki.haskell.org/Literate_programming#Bird_Style>`_
-  # is not supported.
-  'Haskell':        ( '--',     '{-',            '-}'),
-  # ``(*`` ~ ``*)`` not supported.
-  'Delphi':         ( '//',      '{',             '}'),
-  'AppleScript':    ( ('--', '#'), '(*',            '*)'),
-  'Common Lisp':    ( ';',      '#|',            '|#'),
-  # ``--[=[`` ~ ``]=]`` not supported.
-  'Lua':            ( '--',   '--[[',            ']]'),
-  'Clojure':        ( ';',        '',              ''),
-  'Scheme':         ( ';',        '',              ''),
-  'HTML':           (  '',    '<!--',           '-->'),
-  'MXML':           (  '',   '<!---',           '-->'),
-  'Fortran':        ( '!',        '',              ''),
-  'APL':            ( '⍝',        '',              ''),
-  'Makefile':       ( '#',        '',              ''),
-  'RPMSpec':        ( '#',        '',              ''),
-  'Nimrod':         ( '#',        '',              ''),
-  # Comments continued on the next line with a ``\`` aren't supported.
-  'NSIS':           ( ('#', ';'), '',              ''),
-  'TeX':            ( '%',        '',              ''),
-  'Erlang':         ( '%',        '',              ''),
-  'QBasic':         ( "'",        '',              ''),
-  'VB.net':         ( "'",        '',              ''),
-  'REBOL':          ( ';',        '',              ''),
-  'LLVM':           ( ';',        '',              ''),
-  'INI':            ( ';',        '',              ''),
-  'Ada':            ( '--',       '',              ''),
-  'Eiffel':         ( '--',       '',              ''),
-  'vhdl':           ( '--',       '',              ''),
-  # ``*>`` as an inline comment is not supported.
-  # six spaces will precede '*' when converting reST to code
-  ## Six ignored characters followed by * or /.
-  'COBOL':          ('      *',   '',              ''),
-  'YAML':           ( '#',        '',              ''),
+    "Ruby":           ( "#",  "=begin",          "=end"),
+    "S":              ( "#",        "",              ""),
+    # `Bird style <https://wiki.haskell.org/Literate_programming#Bird_Style>`_
+    # is not supported.
+    "Haskell":        ( "--",     "{-",            "-}"),
+    # ``(*`` ~ ``*)`` not supported.
+    "Delphi":         ( "//",      "{",             "}"),
+    "AppleScript":    ( ("--", "#"), "(*",         "*)"),
+    "Common Lisp":    ( ";",      "#|",            "|#"),
+    # ``--[=[`` ~ ``]=]`` not supported.
+    "Lua":            ( "--",   "--[[",            "]]"),
+    "Clojure":        ( ";",        "",              ""),
+    "Scheme":         ( ";",        "",              ""),
+    "HTML":           (  "",    "<!--",           "-->"),
+    "MXML":           (  "",   "<!---",           "-->"),
+    "Fortran":        ( "!",        "",              ""),
+    "APL":            ( "⍝",        "",              ""),
+    "Makefile":       ( "#",        "",              ""),
+    "RPMSpec":        ( "#",        "",              ""),
+    "Nimrod":         ( "#",        "",              ""),
+    # Comments continued on the next line with a ``\`` aren"t supported.
+    "NSIS":           ( ("#", ";"), "",              ""),
+    "TeX":            ( "%",        "",              ""),
+    "Erlang":         ( "%",        "",              ""),
+    "QBasic":         ( "'",        "",              ""),
+    "VB.net":         ( "'",        "",              ""),
+    "REBOL":          ( ";",        "",              ""),
+    "LLVM":           ( ";",        "",              ""),
+    "INI":            ( ";",        "",              ""),
+    "Ada":            ( "--",       "",              ""),
+    "Eiffel":         ( "--",       "",              ""),
+    "vhdl":           ( "--",       "",              ""),
+    # ``*>`` as an inline comment is not supported.
+    # six spaces will precede "*" when converting reST to code
+    ## Six ignored characters followed by * or /.
+    "COBOL":          ("      *",   "",              ""),
+    "YAML":           ( "#",        "",              ""),
 
-  # These languages have failing unit tests
-  # ---------------------------------------
-  # None at this time.
-  }
+# These languages have failing unit tests
+# ---------------------------------------
+# None at this time.
+}
 
 # Supported extensions
 # ====================
